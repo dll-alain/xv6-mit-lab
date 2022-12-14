6aded22a2b1bbfd4ad16e5ae200c00a1bd0ebf95
Alain <18817636063@163.com>
2022-12-14 17:15:21 +0800

    lab1

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,51 +1,42 @@
-//
-// Created by Alain Dong on 12/13/22.
-//
+// user/pingpong.c
 #include "kernel/types.h"
-#include "user/user.h"
 #include "kernel/stat.h"
+#include "user/user.h"
 
 int main(int argc, char *argv[]) {
     int p1[2], p2[2];
-    pipe(p1);
-    pipe(p2);
+    pipe(p1), pipe(p2);
+    char buf[5]; // 用于保存从管道读取的信息
     int size;
-    char buf[5];
     int pid = fork();
     if (pid == 0) {
-        //child process
-        //receive ping
-        //0 read 1 write
-        close(p1[1]);
-        if ((size = read(p1[0], buf, sizeof buf)) > 0) {
-            printf("%d: receive ping", getpid());
+        //读取父进程传过来的信息
+        close(p1[1]); // 关闭管道1的写端
+        if ((size = read(p1[0], buf, sizeof buf)) > 0) { // 从管道1读取不大于buf个字节的数据到buf
+            printf("%d: received ", getpid());
             write(1, buf, size);
+        } else {
+            printf("%d: receive failed\n", getpid());
         }
-        else {
-            printf("%d: receive failed", getpid());
-        }
-        //send pong
-        close(p2[0]);
-        write(p2[1], "pong\n", 5);
+        //向父进程写信息
+        close(p2[0]); // 关闭管道2的读端
+        write(p2[1], "pong\n", 5); // 向管道2写从“pong\n"开始的不大于5个字节的数据
         exit(0);
-    }
-    else if (pid > 0) {
-        //parent process
-        //receive pong
-        close(p2[1]);
-        if ((size = read(p2[0], buf, sizeof buf)) > 0) {
-            printf("%d: receive pong", getpid());
-            write(1, buf, size);
-        }
-        else {
-            printf("%d: receive failed", getpid());
-        }
-        //send ping
+    } else if (pid > 0) {
+        //向子进程写信息
         close(p1[0]);
         write(p1[1], "ping\n", 5);
-        exit(0);
-    }
-    else {
+
+        wait(0);
+        //读取子进程传过来的信息
+        close(p2[1]);
+        if ((size = read(p2[0], buf, sizeof buf)) > 0) {
+            printf("%d: received ", getpid());
+            write(1, buf, size);
+        } else {
+            printf("%d: receive failed\n", getpid());
+        }
+    } else {
         printf("fork error\n");
     }
     exit(0);
